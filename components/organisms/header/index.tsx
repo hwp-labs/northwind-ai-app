9f6ede69ab70753862a57aed6e3c3381f331b666
Emmanuel Tugbeh, MNCS <41299045+2gbeh@users.noreply.github.com>
2026-02-02 02:01:44 +0100

    chore(header): mobile ui clean ups

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../../atoms/logo";
 
 export const Header = () => {
   return (
-    <header className="border_ lg:pl-8 lg:pt-8 pl-4 pt-4">
+    <header className="border_ lg:pl-8 lg:pt-8 pl-6 pt-6">
       <Logo />
     </header>
   );
