aaf60d74b0fc4aaf35e8a309882d7f2c2b211724
Emmanuel Tugbeh, MNCS <41299045+2gbeh@users.noreply.github.com>
2026-02-02 01:52:01 +0100

    chore: mobile ui clean ups

--- components/organisms/status-bar.tsx
+++ components/organisms/status-bar.tsx
@@ -3,7 +3,7 @@ import { APP } from "@/constants/APP";
 export const StatusBar = () => {
   return (
     <section className="flex-center-center mt-10 px-4">
-      <div className="flex-center-start gap-4 rounded-lg border px-4 py-3 font-mono text-xs font-semibold leading-4">
+      <div className="flex-center-start gap-4 rounded-lg border px-4 py-3 font-mono text-xs font-semibold leading-5">
         <span className="text-contrast text-md leading-0 font-bold">&gt;_</span>
         How can {APP.name} help automate my business?
       </div>
